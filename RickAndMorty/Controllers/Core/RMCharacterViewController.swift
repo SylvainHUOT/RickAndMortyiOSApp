43c5876b461ab95b350ec1df6db9f6213893826e
Sylvain HUOT <sylvain.huot@me.com>
2023-01-02 14:45:32 +0100

    Add documents and API Files

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+
+/// Controller to show and search characters
 final class RMCharacterViewController: UIViewController {
 
     override func viewDidLoad() {
--- RickAndMorty/Controllers/Core/RMTabBarController.swift
+++ RickAndMorty/Controllers/Core/RMTabBarController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+
+/// Controller to house tabs an root tab controllers
 final class RMTabBarController: UITabBarController {
 
     override func viewDidLoad() {
