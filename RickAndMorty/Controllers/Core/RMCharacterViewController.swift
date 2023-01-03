f891407aac75adaaff03247ff56be81b1dc6934d
Sylvain HUOT <sylvain.huot@me.com>
2023-01-03 16:28:18 +0100

    API Request

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 /// Controller to show and search characters
 final class RMCharacterViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(
+            endpoint: .character,
+            queryParameters: [
+                URLQueryItem(name: "name", value: "Rick"),
+                URLQueryItem(name: "status", value: "alive")
+            ]
+        )
+        print(request.url)
     }
     
 }
